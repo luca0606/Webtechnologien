d1810eebadddd77aa227ed037718c24ea210d939
man-to-wong <man-to.wong@hotmail.de>
2024-01-31 19:10:17 +0100

    Startseite + Routing implementiert

---
+++ recrutingplatform/src/app/anmeldung/anmeldung.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anmeldung',
+  templateUrl: './anmeldung.component.html',
+  styleUrls: ['./anmeldung.component.scss']
+})
+export class AnmeldungComponent {
+
+}
