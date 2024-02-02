6b9db8ac8f1c1a0db5027dd466e36f5157433bf8
man-to-wong <man-to.wong@hotmail.de>
2024-02-02 18:24:53 +0100

    AnmeldeSeite implementiert

--- recrutingplatform/src/app/anmeldung/anmeldung.component.ts
+++ recrutingplatform/src/app/anmeldung/anmeldung.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'; //Für Formulare 
+import { Router } from '@angular/router'; //Router für Weiterleitung nach Submit
+
 
 @Component({
   selector: 'app-anmeldung',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./anmeldung.component.scss']
 })
 export class AnmeldungComponent {
+  isRecruiter = false;
 
+  /*Formular*/
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
+
+  //Router instanziieren
+  constructor(private router: Router) {
+    console.log('Router wurde instanziiert!')
+  }
+
+  onSubmit(): void {
+    // Hier kommt der Service-Aufruf, um Email bzw. Benutzer und Passwort abzugleichen -> Serviceaufruf(email,pw,isRecruiter) mit Rückgabewert true/false
+    //Zugriff auf Email über: this.loginForm.value.email
+    //Zugriff auf Passwort über: this.loginForm.value.password
+
+    let successLogin = true;//ServieceAufruf
+
+    /*Dummy-Logik*/
+    if (this.loginForm.value.email == 'man-to.wong@hotmail.de' && this.loginForm.value.password == 'manto' && this.isRecruiter == false) {
+      this.router.navigate(['/']); //Routing zur Recruiter bzw. Bewerberplattform
+    }
+    else {
+      alert('Falsche Eingabe!')
+      window.location.reload();
+    }
+  }
+
+  toggleRole(): void {
+    console.log(this.isRecruiter);
+    this.isRecruiter = !this.isRecruiter;
+  }
 }
