54975420de79714a6648e94cd2a52cc28c8d6806
man-to-wong <man-to.wong@hotmail.de>
2024-02-25 12:50:02 +0100

    25.02 13Uhr

---
+++ backend/services/job-service.js
@@ -0,0 +1,8 @@
+const JobModel = require("../models/job")
+const BaseService = require("./base-service")
+
+class JobService extends BaseService {
+
+}
+
+module.exports = new JobService(JobModel)
--- recrutingplatform/src/app/service/stellen.service.spec.ts
+++ recrutingplatform/src/app/service/stellen.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 
-import { StellenService } from './stellen.service';
 
 describe('StellenService', () => {
   let service: StellenService;
