3733b394f405525a6eb36389b4f76e0669d6fc72
man-to-wong <man-to.wong@hotmail.de>
2024-02-25 18:03:37 +0100

    StellenComponent, StellenService, jobSchema

--- recrutingplatform/src/app/service/stellen.service.spec.ts
+++ recrutingplatform/src/app/service/stellen.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+import { StellenService } from './stellen.service';
 
 describe('StellenService', () => {
   let service: StellenService;
--- recrutingplatform/src/app/service/stellen.service.ts
+++ recrutingplatform/src/app/service/stellen.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StellenService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  //Funktionen für Bewerbersicht
+
+  getJobList(): Observable<any> {
+    // Dies gibt ein Observable zurück, das von der aufrufenden Stelle abonniert werden kann
+    return this.http.get("http://localhost:3000/job/");
+  }
+
+
+  // Funktionen für Recruitersicht
+  addJob(jobTitle:String, jobDescription:String, jobRequirements:String, location:String, benefits:String, salaryRangeMin:Number, salaryRangeMax:Number, vacancyActive:Boolean){
+
+    let job = {
+      benefits: benefits,
+      jobDescription: jobDescription,
+      jobRequirements: jobRequirements,
+      jobTitle: jobTitle,
+      location: location,
+      salaryRangeMin: salaryRangeMin,
+      salaryRangeMax: salaryRangeMax,
+      vacancyActive: vacancyActive
+  }
+
+
+
+  console.log(job);
+  this.http.post("http://localhost:3000/job/", job)
+      .subscribe((res) => {
+        return true;
+        console.log(res);
+      });
+
+  }
+
+  deleteJob(){
+  }
+
+  
+
 }
