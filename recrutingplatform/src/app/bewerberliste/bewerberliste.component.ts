5459a09a039d95c4c55dd49b204034d26072e1de
man-to-wong <man-to.wong@hotmail.de>
2024-02-25 01:07:41 +0100

    Routing für Registrierungscomponent

---
+++ recrutingplatform/src/app/bewerberliste/bewerberliste.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bewerberliste',
+  templateUrl: './bewerberliste.component.html',
+  styleUrls: ['./bewerberliste.component.scss']
+})
+export class BewerberlisteComponent {
+
+}
