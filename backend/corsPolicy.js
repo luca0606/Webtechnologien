0d1ec0c780060ea1261d4d7f77a66bdcf68862ce
Birol Yilmaz <brlylmz23@gmail.com>
2024-03-03 08:26:54 +0100

    anmeldung service backend

--- backend/corsPolicy.js
+++ backend/corsPolicy.js
@@ -1,21 +1,21 @@
 const cors = require("cors")
 // define allowed urls to make request from this api. 
-const whitelist = ["http://localhost:4200"]
+const whitelist = ["http://localhost:4200", "http://localhost:56610", "http://localhost:56610"]
 const corsOptions = {
 
     // To get rid of url problem for development phase. 
     // Commented code will be uncommented before submission.
-    origin: "*"
+    // origin: "*"
 
-    // origin: function (origin, callback) {
-    //     console.log("huuuu backend")
-    //     if (!origin || whitelist.indexOf(origin) !== -1) {
-    //         callback(null, true)
-    //     } else {
-    //         console.log("cors error")
-    //         callback(new Error('Not allowed by CORS'))
-    //     }
-    // }
+    origin: function (origin, callback) {
+        console.log("huuuu backend")
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            console.log("cors error")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
 }
 const PORT = process.env.PORT || 3000;
 module.exports = app => {
--- backend/routes/auth-router.js
+++ backend/routes/auth-router.js
@@ -4,6 +4,28 @@ const AuthModel = require("../models/auth")
 
 class AuthRouter extends BaseRouter {
 
+    constructor(AuthModel, AuthService) {
+        super(AuthModel, AuthService)
+
+        this.router.post("/login", async (req, res) => {
+
+            const { email, password } = req.body;
+
+            try {
+                const token = await this.service.authenticate(email, password)
+                // Jwt token can be used for each request if it is needed.
+                if (token) {
+                    res.status(200).send(true);
+                }
+            } catch (error) {
+                return res.status(500).send(error)
+            }
+
+        });
+
+    }
+
+
 }
 
 module.exports = new AuthRouter(AuthModel, AuthService).router
